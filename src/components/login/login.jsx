55892564970eb92d5eee5da3ecf54e75ac4c4773
Helmik Escamilla <hescamilla@itexico.net>
2017-04-26 17:25:41 -0500

    start to develop login

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -5,36 +5,48 @@ import hamburguerImg from './assets/images/hamburguer.jpg'
 import meatImg from './assets/images/meat.jpg'
 import pizzaImg from './assets/images/pizza.jpg'
 import wingsImg from './assets/images/wings.jpg'
-import { Button } from 'reactstrap';
+import { Button, Input, FormGroup, Label } from 'reactstrap';
 
 export default class Login extends Component {
     constructor() {
         super();
         const images = [hamburguerImg, meatImg, pizzaImg, wingsImg];
         this.state = {
-            image: pizzaImg
+            image: pizzaImg,
+            username: '',
+            password: ''
         }
         setInterval(() => this.selectImage(images), 3500);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.login = this.login.bind(this);
     }
 
     render() {
         return (
             <div className="container" style={{backgroundImage: 'url('+ this.state.image + ')'}}>
                 <div className="login-container">
-                    <div className="center-child">
-                        <label className="text-content">User:</label>
-                        <input type="text"/>
-                    </div>
-                    <div className="center-child">
-                        <label className="text-content">Password:</label>
-                        <input type="password"/>
-                    </div>
+                    <FormGroup>
+                        <Label>Username</Label>
+                        <Input placeholder="Username" value={this.state.username} onChange={this.onChangeUsername}/>
+                        <Label>Password</Label>
+                        <Input type="password" placeholder="Password" onClick={this.login} value={this.state.password} onChange={this.onChangePassword}/>
+                    </FormGroup>
                     <div>
-                        <Button color="primary" size="sm">Login</Button>
+                        <Button color="primary" onClick={this.login}>Login</Button>
                     </div>
                 </div>
             </div>
         );
+    }
+    onChangePassword(event) {
+        this.setState({password: event.target.value});
+    }
+    onChangeUsername(event) {
+        this.setState({username: event.target.value});
+    }
+    login(evemt) {
+        console.log(this.state);
     }
 
     selectImage(imagesArray) {
